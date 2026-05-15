4f8601d8c9784557f5ea853b7394716c841c508a
heyewuyue1 <jasonhejiahao2001@126.com>
2026-05-15 12:43:31 +0000

    Deploying to gh-pages from @ heyewuyue1/heyewuyue1.github.io@fc38d2c21c6cd6c178b5c227b9fa0293a930c0e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "Repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
